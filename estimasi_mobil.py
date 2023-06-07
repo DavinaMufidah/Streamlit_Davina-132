1a3d6185554a295aea7797e0cc7eb8c9418aaafe
DavinaMufidah <128565952+DavinaMufidah@users.noreply.github.com>
2023-06-07 08:55:33 +0700

    Update estimasi_mobil.py

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -1,8 +1,6 @@
 import pickle
 import streamlit as st
 
-model = pickle.load(open('estimasi_mobil.sav', 'rb'))
-
 st.title('Estimasi Harga Mobil Bekas')
 
 year = st.number_input('Input Tahun Mobil')
